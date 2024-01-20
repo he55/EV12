7d73dc8d2318215f74eb30afb12461ad08ea9b3b
he55 <40764272+he55@users.noreply.github.com>
2024-01-21 00:29:39 +0800

    12

--- MacWallpaper/Class2.cs
+++ MacWallpaper/Class2.cs
@@ -7,6 +7,18 @@ using System.Runtime.CompilerServices;
 
 namespace MacWallpaper
 {
+    public class Emoji2
+    {
+        public string cldr { get; set; }
+        public string fromVersion { get; set; }
+        public string glyph { get; set; }
+        public string group { get; set; }
+        public string[] keywords { get; set; }
+        public string[] mappedToEmoticons { get; set; }
+        public string tts { get; set; }
+        public string unicode { get; set; }
+    }
+
     public class EmojiCategory
     {
         public string title { get; set; }
